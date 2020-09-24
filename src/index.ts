55c0b470b1e2a645caead3527a002c4bf3ad585b
nicoleczhu <nicolezhu@google.com>
2020-09-24 14:30:44 -0700

    Add: Github workflow config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
+  // TODO start listening to workflow events
   app.on('issues.opened', async (context) => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     await context.github.issues.createComment(issueComment)
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
